183ab196639ce32c1496deb5c54059c55dc54522
854098072@qq.com <854098072@qq.com>
2019-05-22 22:28:18 +0800

    init

--- MRedrockExam2019/app/src/main/java/com/override0330/android/redrock/mredrockexam2019/model/music/MyMusic.kt
+++
@@ -1,5 +0,0 @@
-package com.override0330.android.redrock.mredrockexam2019.model.music
-
-import com.override0330.android.redrock.mredrockexam2019.mymusic.MusicPrepare
-
-class MyMusic(val name:String, val author:String, val imageUrl:String,val id:String,val musicPrepare: MusicPrepare)
